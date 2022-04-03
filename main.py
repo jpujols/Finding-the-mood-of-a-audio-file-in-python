b3c1c79c13eb07699ff2efc5180fba2887a458d1
jpujols <54540355+jpujols@users.noreply.github.com>
2022-04-03 17:31:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+#Importing libraries
 from speech_recognition import Recognizer, AudioFile
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 recognizer = Recognizer()
 
+#Loading audio file
 with AudioFile('chile.wav') as audio_file:
   audio = recognizer.record(audio_file)
 
 text = recognizer.recognize_google(audio)
 
+#db contains words with sentiment
 nltk.download('vader_lexicon')
 
 analyzer = SentimentIntensityAnalyzer()
